be2cdd668e58308c85beecb7408d0c9ff76e121c
EDWIN AU <edwinau@rogers.com>
2017-05-03 20:59:55 -0400

    cats summons successfully

--- cats.js
+++ cats.js
@@ -1,15 +1,23 @@
 $(function() {
 
   $('.summon-cats').on('click', function() {
-    url = 'http://bitkittens.herokuapp.com/cats.json'
+    url = 'http://bitkittens.herokuapp.com/cats.json',
     $.ajax({
       url: url,
       method: 'GET',
       // data:
       dataType: 'json'
     }).done(function(data) {
-      console.log("success!")
-    })
+      var cats = data.cats;
+      var catsArray = [];
+      for (var i = 0; i < cats.length; i++) {
+        var source = cats[i]['photo'];
+        var image = $('<img>').attr('src', source).appendTo('#cat' + (i + 1));
+        catsArray.push(image);
+      }
+    $('.cat-box').each(function(i){
+    $(this).html(catsArray[i]);
   });
-
+});
+});
 });
